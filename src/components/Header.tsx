f8af1816b1effbc30ad895efcdd9f12c86b7436e
eedu7 <mueedahmad067@gmail.com>
2024-12-17 12:59:26 +0500

    Formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import HeaderImage from "@/components/HeaderImage";
 
 const Header = () => {
     return (<header>
-            <div className="fixed z-10 w-full flex justify-between items-center px-12 py-6">
-                <div>
-                    <h1 className="text-slate-50 font-semibold text-2xl tracking-widest font-mono">Bisal Ahmad</h1>
-                </div>
-                <Navbar/>
+        <div className="fixed z-10 w-full flex justify-between items-center px-12 py-6">
+            <div>
+                <h1 className="text-slate-50 font-semibold text-2xl tracking-widest font-mono">Bisal Ahmad</h1>
             </div>
-            <HeaderImage />
-        </header>)
+            <Navbar/>
+        </div>
+        <HeaderImage/>
+    </header>)
 }
 export default Header
