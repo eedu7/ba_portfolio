0c4b061eadf5b1cfee7dbc02f3495d830d255f3b
eedu7 <mueedahmad067@gmail.com>
2024-12-19 16:41:07 +0500

    FEAT: Made the Bio component more responsive

--- src/app/(home)/components/Bio.tsx
+++ src/app/(home)/components/Bio.tsx
@@ -5,14 +5,14 @@ import BackgroundImage from "../../../../public/images/header_background.jpg";
 const Bio = () => {
     return (
         <div
-            className="w-full h-[400px] bg-gray-950 ">
-            <div className="max-w-screen-lg h-full mx-auto grid grid-cols-2 content-center px-8 text-white gap-4">
-                <div className="grid-cols-1 lg:w-[451px] lg:h-[301px]">
-                    <Image src={BackgroundImage} alt="Profile Image" className="w-full h-full object-cover"/>
+            className="w-full py-4 bg-gray-950 ">
+            <div className="max-w-screen-lg h-full mx-auto flex flex-col justify-center md:grid grid-cols-2 content-center px-8 text-white gap-4">
+                <div className="grid-cols-1">
+                    <Image src={BackgroundImage} alt="Profile Image" className="w-full h-full object-fit"/>
                 </div>
                 <div className="grid-cols-1 gap-4 space-y-4">
-                    <h1 className="text-4xl text-center font-bold font-mono tracking-widest">Meet Bisal</h1>
-                    <p className="font-sans ">Bisal is a versatile marketing professional experienced in each
+                    <h1 className="text-[32px] lg:text-[32px] md:text-[48px] text-center font-bold font-abril_fatface">Meet Bisal</h1>
+                    <p className="font-bird_eye lg:text-[16px] text-center ">Bisal is a versatile marketing professional experienced in each
                         stage of the marketing and sales process. He is a thorough researcher skilled in
                         identifying target markets and customer insights to inform effective marketing programs.
                         Bisal uses an adept eye for design and powerful writing and public speaking skills to
